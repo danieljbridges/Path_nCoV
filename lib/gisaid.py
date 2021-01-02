6ef58913eaa21f7df5b9e99442f5132a68c7cab1
JasonAHendry <jason.ahendry@gmail.com>
2021-01-02 05:28:11 +0000

    Computing statistics for GISAID submission in python pipeline. (#2)
    * Reorganised notebooks.
    
    * Reorganised notebooks.
    
    * IPython version change in the notbook; not sure why.
    
    * Library to compute GISAID statistics
    
    * Script to compute GISAID statistics
    
    * Accommodating C02 folder structure.
    
    * Fixing file path.
    
    * Quick look at GISAID stats.
    
    * Functions for computing sequencing depth from the fastq files.
    
    * Need to import subprocess module.
    
    * Updated to compute average sequencing depth from fastq files.
    
    * Artic not Arctic. Plus removed commented section.
    
    * Removed C02 for now, as there are issues with the sample list.
    
    * Newline.
    
    * Comment before examine.
    
    * Made loading of sample list robust to trailing whitespace.
    
    * Added comparison between fastq and ARTIC coverage sequencing depth.

---
+++ lib/gisaid.py
@@ -0,0 +1,299 @@
+# ------------------------------------------------------------------------------
+# Functions to compute statistics required for GISAID subsmission
+# JHendry, 2021/01/01
+# ------------------------------------------------------------------------------
+
+import os
+import sys
+import subprocess
+import pandas as pd
+import numpy as np
+
+
+def load_coverage_files(sample_dir):
+    """
+    Load coverage files from ARCTIC output
+    
+    Parameters
+        sample_dir : str
+            Path to the sample directory. Within this
+            directory there should be coverage files
+            generated from the ARCTIC pipeline which
+            have a '.depths' suffix.
+            
+    Returns
+        df : DataFrame, shape (2, n_sites)
+            A data frame of two columns containing
+            genomic position 'pos' and coverage 'cov'
+            data. Each position is representd only once;
+            the coverage across multiple primer pools is summed.
+    
+    """
+    
+    # Identify coverage files
+    cov_files = [fn for fn in os.listdir(sample_dir) if fn.endswith(".depths")]
+    
+    # Prepare storage
+    dt = {
+        "pos": [],
+        "cov": []
+    }
+
+    # Iterate over pools
+    for cov_fn in cov_files:
+
+        # Open file
+        fn = os.path.join(sample_dir, cov_fn)
+        with open(fn, "r") as f:
+
+            # Iterate over lines
+            for line in f:
+
+                # Parse line
+                _, _, pos, cov = line.strip().split("\t")
+
+                # Store results
+                dt["pos"].append(int(pos))  # note data type coercion
+                dt["cov"].append(int(cov))  # ""
+    
+    # Convert to data frame
+    df = pd.DataFrame(dt)
+    
+    # Get total coverage at each position
+    df = (df
+          .groupby("pos")
+          .sum()
+          .reset_index()
+         )
+    
+    return df
+
+
+def load_sample_list(fn):
+    """
+    Load Sample List into Python
+    as a dictionary which maps sample
+    names to barcodes
+    
+    Parameters
+        fn : str
+            Path to SampleList file.
+    
+    Returns
+        dt : dict
+            Maps sample name to barcode.
+    
+    """
+    
+    with open(fn, "r") as f:
+        dt = {}
+        for line in f:
+            if not line.isspace():
+                barcode, sample = line.rstrip().split("\t")
+                dt[sample] = int(barcode)
+    
+    return dt
+
+
+def get_contig_lengths(cov):
+    """
+    Given a coverage profile, return an array
+    giving the list of all the contigs
+    
+    In this context a contig is defined as a contiguous
+    region of sequence with a coverage > 0X.
+    
+    Parameters
+        cov : array, int, shape (n_sites)
+            Coverage along the genome; number of reads
+            covering each consecutive nucleotide.
+            
+    Returns
+        ll : ndarray, int, shape(n_contigs)
+            An array containing the length of each
+            of the contigs
+            
+    """
+    
+    contigs = []
+    l = 0
+    for covered in cov > 0:
+        if covered:
+            l += 1
+        elif l > 0:
+            contigs.append(l)
+            l = 0
+    
+    if l > 0: 
+        contigs.append(l)  # append last segment
+
+    return np.array(contigs)
+
+
+def calc_contig_n50(cov):
+    """
+    Compute the N50 contig length
+    
+    Parameters
+        cov : array, int, shape (n_sites)
+            Coverage along the genome; number of reads
+            covering each consecutive nucleotide.
+            
+    Returns
+        _ : int
+            The N50 contig length; 50% of the total
+            contig length is contained in contigs
+            of this length or smaller.
+
+    """
+    
+    contigs = get_contig_lengths(cov)
+    
+    if len(contigs) > 0:
+        contigs.sort()
+        total_contig = contigs.sum()
+        cum_contig_frac = (contigs / total_contig).cumsum()
+        ix = np.argmax(cum_contig_frac > 0.5)
+        
+    else:
+        return np.nan
+    
+    return contigs[ix]
+
+
+def calc_gisaid_stats(df, depth_threshold=10):
+    """
+    Calculate bioinformatics statistics required for 
+    GISAD submission
+    
+    Parameters
+        df : DataFrame, shape (2, n_sites)
+            Pandas DataFrame containing two columns,
+            one for genomic position "pos" and one for
+            coverage "cov". Rows are consecutive
+            sites in the genome.
+        depth_threshold : int, optional
+            Used to calculate breadth.
+            
+    Returns
+        dt : dict
+            Dictionary containing all GISAID QC
+            statistics, excluding 'Ns per 100 kbp'
+            
+    """
+    
+    # Reference genome length
+    genome_length = df["pos"].max() - df["pos"].min()
+    
+    # Consensus genome length
+    covered = df["cov"] > 0
+    consensus_genome_length = df[covered]["pos"].max() - df[covered]["pos"].min() + 1
+    
+    # Total base pairs sequenced
+    # NB: this is post-mapping and depth truncation
+    number_base_pairs = df["cov"].sum()
+    
+    # Sequence depth (average)
+    sequencing_depth_avg = number_base_pairs / genome_length
+    
+    # Coverage depth (average)
+    coverage_depth = number_base_pairs / consensus_genome_length
+    
+    # Assembly coverage breadth (as percentage)
+    coverage_breadth = 100 * (df["cov"] >= depth_threshold).sum() / genome_length
+    
+    # Mean contig length
+    contigs = get_contig_lengths(df["cov"])
+    mean_contig_length = contigs.mean()
+    
+    # N50 contig length
+    n50_contig_length = calc_contig_n50(df["cov"])
+    
+    
+    # Package key statistics into a dictionary
+    dt = {
+        "sequencing_depth_avg": sequencing_depth_avg,
+        "coverage_breadth": coverage_breadth,
+        "assembly_coverage_depth": coverage_depth,
+        "number_base_pairs": number_base_pairs,
+        "consensus_genome_length": consensus_genome_length,
+        "ref_genome_length": genome_length,
+        "mean_contig_length": mean_contig_length,
+        "N50": n50_contig_length
+    }
+    
+    return dt
+
+
+def calc_ns_per_100kbp(consensus_fasta, verbose=True):
+    """
+    Calculate the number of Ns per 100kbp from
+    a consensus FASTA
+    
+    Parameters
+        consensus_fasta : str
+            Path to the consensus fasta file.
+        verbose : bool
+            Switch for print diagnostics.
+    
+    Returns
+        ns_per_100kbp : float
+            The average number of Ns per 100kbp.
+    
+    """
+    
+    # Open consensus fasta
+    with open(consensus_fasta, "r") as f:
+    
+        # Parse header
+        header = f.readline().strip()
+
+        # Parse sequence
+        seq = ""
+        for line in f:
+            seq += line.strip()
+
+        # Calculate statistics
+        seq_length = len(seq)
+
+    # Number of Ns
+    num_ns = sum([1 for c in seq if c == "N"])
+
+    # Ns per 100 kbp
+    ns_per_100kbp = num_ns / seq_length * 100 * 10 **3
+
+    if verbose:
+        print("FASTA name: %s" % consensus_fasta)
+        print("Header: %s" % header)
+        print("Sequence length (bp): %d" % seq_length)
+        print("Number of Ns: %d" % num_ns)
+        print("Ns per 100kbp: %.02f" % ns_per_100kbp)
+        
+    return ns_per_100kbp
+
+
+def calc_avg_seq_depth(fastq_fn, genome_length):
+    """
+    Calculate average sequencing depth, given
+    a fastq file named `fastq_fn` and a
+    `genome_length`
+    
+    Parameters
+        fastq_fn : str
+            Path to fastq file.
+        genome_length : int
+            Length of the genome from which the
+            reads in the fastq were generated.
+            
+    Returns
+        _ : int
+            Average sequencing depth given all
+            the data within the fastq.
+    
+    """
+    
+    # Code golf style
+    cmd = "cat %s | paste - - - - | cut -f2 | tr -d '\n' | wc -c" % fastq_fn
+    total_bp = int(subprocess.check_output(cmd, shell=True))
+    return total_bp / genome_length
+
---
+++ run_gisaid-statistics.py
@@ -0,0 +1,176 @@
+# Compute statistics for GISAID submission
+# ----------------------------------------
+#
+# Example usage:
+#   python run_gisaid-statistics.py -d data/WGS
+#
+# JHendry, 2021/01/01
+
+
+import os
+import sys
+import subprocess
+import time
+import datetime
+import getopt
+import pandas as pd
+import numpy as np
+from lib.gisaid import *
+
+
+print("=" * 80)
+print("Compute GISAID statistics")
+print("-" * 80)
+print("Command: %s" % " ".join(sys.argv))
+print("Run on host: %s" % os.uname().nodename)
+print("Operating system: %s" % os.uname().sysname)
+print("Machine: %s" % os.uname().machine)
+print("Started at: %s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+print("=" * 80)
+start_time = time.time()
+
+
+# PARSE CLI INPUT
+print("Parsing command line inputs...")
+try:
+    opts, args = getopt.getopt(sys.argv[1:], ":d:")
+except getopt.GetoptError:
+    print("  Error parsing options.")
+for opt, value in opts:
+    if opt == "-d":
+        data_dir = value
+        print("  Data directory: %s" % data_dir)
+    else:
+        print("  Parameter %s not recognized." % opt)
+        sys.exit(2)
+print("Done.")
+print("")
+
+
+# PREPARE DIRECTORIES
+print("Preparing directories...")
+rampart_dir = os.path.join(data_dir, "2_SampleList_and_Rampart")
+artic_dir = os.path.join(data_dir, "3_Artic_Output")
+output_dir = os.path.join("5_GISAID")
+if not os.path.isdir(output_dir):
+    os.makedirs(output_dir)
+print("  Rampart directory: %s" % rampart_dir)
+print("  Artic directory: %s" % artic_dir)
+print("  Output directory: %s" % output_dir)
+print("Done.")
+print("")
+
+
+# EXAMINE CONTENTS
+print("Examining directory contents...")
+contents_dt = {}
+rs = os.listdir(artic_dir)
+rs.remove("C02")
+for r in os.listdir(artic_dir):
+    if os.path.isdir(os.path.join(artic_dir, r)) and r.startswith("C"):
+        if r == "C02":
+            d = os.path.join(artic_dir, r, "Artic/processed")
+        else:
+            d = os.path.join(artic_dir, r, "processed")
+        n_samples = sum([1 for s in os.listdir(d) 
+                         if os.path.isdir(os.path.join(d, s))])
+        contents_dt[r] = n_samples
+print("  Run\tNo. samples")
+for d, n in contents_dt.items():
+    print("  %s\t%d" % (d, n))
+print("  Total runs: %d" % len(contents_dt.keys()))
+print("  Total samples: %d" % sum(contents_dt.values()))
+print("Done.")
+print("")
+
+
+# COMPUTE GISAID STATISTICS
+print("Computing GISAID statistics...")
+
+# Prepare storage
+dts = []
+
+# Iterate over runs
+print("  Run  Samples complete")
+for r in rs:
+    
+    # Define run directory
+    run_dir = os.path.join(artic_dir, r)
+    if os.path.isdir(run_dir) and r.startswith("C"):
+        if r == "C02":
+            d = os.path.join(run_dir, "Artic/processed")
+            slist_fn = os.path.join(rampart_dir, "%s_Artic_SList.txt" % r)
+        else:
+            d = os.path.join(run_dir, "processed")
+            slist_fn = os.path.join(rampart_dir, "%s_SList.txt" % r)
+            
+        # Load sample-barcode map
+        try:
+            sample_dt = load_sample_list(slist_fn)
+        except:
+            print("Failed to load sample list for run %s" % r)
+
+
+        # Iterate over samples
+        n_total = len(os.listdir(d))
+        for i, s in enumerate(os.listdir(d)):
+            
+            # Print progress
+            sys.stdout.write("\r")
+            sys.stdout.flush()
+            sys.stdout.write("  %s %d/%d" % (r, i+1, n_total))
+            
+            # Define sample directory
+            sample_dir = os.path.join(d, s)
+            
+            # Define sample barcode
+            b = sample_dt[s]
+            
+            # Identifiers
+            stats_dt = {}
+            stats_dt["run"] = r
+            stats_dt["sample"] = s
+            stats_dt["barcode"] = b
+
+            # Calc. most statistics from coverage file
+            coverage_df = load_coverage_files(sample_dir)
+            stats_dt.update(calc_gisaid_stats(coverage_df))
+            
+            # Calc. Ns per 100kbp from consensus FASTA
+            consensus_path = os.path.join(sample_dir, "%s.consensus.fasta" % s)
+            ns_per_100kbp = calc_ns_per_100kbp(consensus_path, verbose=False)
+            stats_dt.update({"ns_per_100kbp": ns_per_100kbp})
+            
+            # Calc. sequencing depth from .fastq
+            b_fn = os.path.join(d.replace("processed", "fastq"), "C%d_barcode%.2d.fastq" % (int(r[1:]), b))
+            try:
+                sequencing_depth_avg_fastq = calc_avg_seq_depth(b_fn, genome_length=stats_dt["ref_genome_length"])
+                stats_dt.update({"sequencing_depth_avg_fastq": sequencing_depth_avg_fastq})
+            except:
+                stats_dt.update({"sequencing_depth_avg_fastq": 0})
+            
+            # Store
+            dts.append(stats_dt)
+        
+        sys.stdout.flush()
+        sys.stdout.write("\n")
+        
+# Create data frame
+df = pd.DataFrame(dts)
+print("Done.")
+print("")
+
+
+# WRITE RESULTS
+print("Writing results...")
+output_fn = "%s_gisaid.csv" % datetime.datetime.now().strftime("%Y-%m-%d")
+df.to_csv(os.path.join(output_dir, output_fn), index=False)
+print("  To: %s" % os.path.join(output_dir, output_fn))
+print("Done.")
+print("")
+
+
+print("-" * 80)
+print("Runtime: %s" % str(datetime.timedelta(seconds=time.time() - start_time)))
+print("Finished at: %s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+print("=" * 80)
